6888461f7408d92c06a4620f9f9d6b4b3717bc99
Weyu Team <team@weyu.io>
2021-04-14 18:12:13 +0200

    style email signup form

---
+++ pages/subscribed.vue
@@ -0,0 +1,10 @@
+<template>
+  <section class="section">
+    <div class="container is-max-widescreen">
+      <div class="has-text-centered">
+        <h2 class="title has-text-green">Thanks for subscribing!</h2>
+        <nuxt-link class="button" to="/">Home</nuxt-link>
+      </div>
+    </div>
+  </section>
+</template>
