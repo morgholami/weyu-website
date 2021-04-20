fe3771b797153d2fb3267da7308967b088c1bf42
Weyu Team <team@weyu.io>
2021-04-20 12:04:35 +0200

    add whitelist typeform

--- pages/invest.vue
+++ pages/invest.vue
@@ -1,10 +1,45 @@
 <template>
-  <section class="section">
+  <div>
     <div class="container">
-      <div class="has-text-centered">
-        <h2 class="subtitle is-2">Whitelist opening soon!</h2>
-        <nuxt-link class="button" to="/">Home</nuxt-link>
-      </div>
+      <div ref="typeform" id="typeform">loading..</div>
     </div>
-  </section>
+    <section class="section">
+      <div class="container has-text-centered mt-6">
+        <a href="https://9wx6twk78a7.typeform.com/to/wecvobI4" target="_blank">open whitelist form</a>
+      </div>
+    </section>
+  </div>
 </template>
+
+<script>
+  const typeformEmbed = require('@typeform/embed');
+  export default {
+    mounted() {
+      typeformEmbed.makeWidget(
+              this.$refs.typeform,
+              'https://9wx6twk78a7.typeform.com/to/wecvobI4',
+              {
+                opacity: 50,
+                hideScrollbars: true,
+                hideFooter: true,
+                hideHeader: true,
+                buttonText: "Start Form",
+                onSubmit: function () {
+                  console.log('Typeform successfully submitted')
+                }
+              }
+      )
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  #typeform {
+    height: 600px;
+    width: 100%;
+    background: $white;
+  }
+  .bdziaR {
+    opacity: 1 !important;
+  }
+</style>
