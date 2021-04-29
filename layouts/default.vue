ff7b5e41b7982efeee384199e3166834298af606
Weyu Team <team@weyu.io>
2021-04-29 21:14:41 +0200

    WIP: whitepaper embed

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app" :class="{ 'homepage': $route.path == '/', 'light': $route.path == '/login' }">
+  <div id="app" :class="{ 'homepage': $route.path == '/', 'light': $route.path == '/login' || $route.path == '/whitepaper' }">
     <nav-bar/>
     <div id="content">
       <Nuxt/>
---
+++ pages/whitepaper.vue
@@ -0,0 +1,10 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <div class="has-text-centered">
+        <iframe src="http://docs.google.com/viewer?url=https://weyu.io/download/weyu_whitepaper.pdf&embedded=true" width="100%" height="600" style="width: 100%; height: calc(100vh - 100px);" frameborder="0" scrolling="no"></iframe>
+        <a href="/download/weyu_whitepaper.pdf" target="_blank" class="button">Download Whitepaper</a>
+      </div>
+    </div>
+  </section>
+</template>
