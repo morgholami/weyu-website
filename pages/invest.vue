82b4a7614c9b5e92097fe21442805347168f028e
Weyu Team <team@weyu.io>
2021-04-30 13:28:00 +0200

    add seed round typeform

--- pages/invest.vue
+++ pages/invest.vue
@@ -1,10 +1,51 @@
 <template>
-  <section class="section">
-    <div class="container">
-      <div class="has-text-centered">
-        <h2 class="subtitle is-2">Whitelist opening soon!</h2>
-        <nuxt-link class="button" to="/">Home</nuxt-link>
+  <div>
+    <section class="section">
+        <div class="container">
+          <div class="has-text-centered" style="margin-left: auto; margin-right: auto; max-width: 700px;">
+            <p class="subtitle is-5 block">The WEYU seed round is now open. This round is reserved for influencers and industry experts who have a proven history with working with innovative crypto projects.
+            <p class="subtitle is-5 block">This round will end soon! Apply below:</p>
+          </div>
+          <div ref="typeform" id="typeform">loading..</div>
+        </div>
+    </section>
+    <section class="section">
+      <div class="container has-text-centered mt-6">
+        <a href="https://9wx6twk78a7.typeform.com/to/Yn7icJyU" target="_blank">open form</a>
       </div>
-    </div>
-  </section>
+    </section>
+  </div>
 </template>
+
+<script>
+const typeformEmbed = require('@typeform/embed');
+export default {
+  mounted() {
+    typeformEmbed.makeWidget(
+        this.$refs.typeform,
+        'https://9wx6twk78a7.typeform.com/to/Yn7icJyU',
+        {
+          opacity: 50,
+          hideScrollbars: true,
+          hideFooter: true,
+          hideHeader: true,
+          buttonText: "Start Form",
+          onSubmit: function () {
+            console.log('Typeform successfully submitted')
+          }
+        }
+    )
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+#typeform {
+  height: 600px;
+  width: 100%;
+  background: $white;
+}
+.bdziaR {
+  opacity: 1 !important;
+}
+</style>
