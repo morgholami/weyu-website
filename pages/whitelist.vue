cf3ee50962a1fcae3b91bafd65e7eb035d4556e5
Weyu Team <team@weyu.io>
2021-05-04 20:13:26 +0200

    update whitepaper + whitelist design

--- pages/whitelist.vue
+++ pages/whitelist.vue
@@ -2,10 +2,12 @@
   <div>
     <section class="section">
         <div class="container">
-          <div class="has-text-centered" style="margin-left: auto; margin-right: auto; max-width: 700px;">
-            <p class="subtitle is-5 block">The <b>WEYU Whitelist</b> is now live for the WEYU Open Private Sale.<br>Register now to secure your spot in the sale.</p>
-            <p class="has-text-danger"><b>BEWARE OF SCAMMERS</b></p>
-            <p>We will not ask to transfer funds or ask you to share anymore information until we publicly announce the official opening of the WEYU token sale.</p>
+          <div class="has-text-centered" style="margin-left: auto; margin-right: auto; max-width: 750px;">
+            <p class="subtitle is-4 mb-2 has-text-weight-semibold">The WEYU Whitelist is now live for the WEYU Open Private Sale.</p>
+            <p class="subtitle">Register now to secure your spot in the sale.</p>
+            <p class="has-background-danger-light p-4 has-radius-small" style="max-width: 570px; margin-left: auto; margin-right: auto">
+              <b class="has-text-danger">BEWARE OF SCAMMERS</b><br>
+            <small>We will not ask to transfer funds or ask you to share anymore information until we publicly announce the official opening of the WEYU token sale.</small></p>
             <br>
           </div>
           <div ref="typeform" id="typeform">loading..</div>
