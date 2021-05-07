214f9e8546684ff06e1c7b625c37855a1a095e51
Weyu Team <team@weyu.io>
2021-05-07 13:03:24 +0200

    update whitepaper

--- pages/whitepaper.vue
+++ pages/whitepaper.vue
@@ -2,8 +2,8 @@
   <section class="section">
     <div class="container">
       <div class="has-text-centered">
+        <a href="/download/weyu_whitepaper.pdf" target="_blank" class="button mb-2">Download Whitepaper</a>
         <iframe src="https://docs.google.com/viewer?url=https://weyu.io/download/weyu_whitepaper.pdf&embedded=true" width="100%" height="600" style="width: 100%; height: calc(100vh - 150px); min-height: 500px;" frameborder="0" scrolling="no"></iframe>
-        <a href="/download/weyu_whitepaper.pdf" target="_blank" class="button">Download Whitepaper</a>
       </div>
     </div>
   </section>
