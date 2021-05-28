fb9ecb96acee89b4de3afcf8cd4341edbf41b907
Weyu Team <team@weyu.io>
2021-05-28 02:10:56 +0200

    WIP: account page

--- pages/account.vue
+++ pages/account.vue
@@ -1,9 +1,42 @@
 <template>
   <section class="section">
     <div class="container ">
-      <div v-if="loading">Loading account..</div>
+      <div v-if="loading">Loading..</div>
       <div v-if="user">
-        {{ user }}
+        <a :href="$bsc.explorer + '/address/'+ user.address" target="_blank"
+           class="blockchain-address">{{ user.address }}</a>
+        <div v-if="user.email && !editEmail">
+          {{user.email}} <a @click="email = user.email; editEmail = true">edit</a>
+        </div>
+        <div v-else>
+          <form @submit.prevent="setEmail">
+            <div class="field has-addons">
+              <div class="control">
+                <input type="email" required v-model="email" class="input" placeholder="Your email" />
+              </div>
+              <div class="control is-flex is-align-items-center">
+                <button type="submit" class="button is-primary" :disabled="!email">
+                  Set email
+                </button>
+                <a v-if="editEmail" @click="editEmail = false" class="ml-2">cancel</a>
+              </div>
+            </div>
+          </form>
+        </div>
+        <div v-if="user.telegram">
+          {{user.telegram}}
+        </div>
+        <vue-telegram-login
+            mode="callback"
+            telegram-login="YourTelegramBot"
+            requestAccess="write"
+            @callback="telegramLogin" />
+        <div>
+          <h2>Referral Link</h2>
+          <div class="control">
+            <input class="input" type="text" :value="'https://weyu.io?ref='+user.referral_code" readonly>
+          </div>
+        </div>
         <div class="has-text-centered">
           <a class="button is-danger" @click="$bsc.logout()">
             Logout
@@ -16,16 +49,20 @@
 </template>
 
 <script>
+import {vueTelegramLogin} from 'vue-telegram-login'
   export default {
     computed: {
       loggedIn () {
         return (this.$bsc && this.$bsc.token)
       }
     },
+    components: {vueTelegramLogin},
     data() {
       return {
         loading: false,
-        user: null
+        user: null,
+        email: null,
+        editEmail: false
       }
     },
     created () {
@@ -37,6 +74,22 @@
       }
     },
     methods: {
+      telegramLogin (user) {
+        console.log(user)
+      },
+      async setEmail() {
+        this.loading = true
+        try {
+          const response = await this.$axios.post('/user', {
+            email: this.email
+          })
+          this.user = response.data;
+        } catch (e) {
+          alert("Could not update account")
+        }
+        this.editEmail = false
+        this.loading = false
+      },
       async getUser() {
         this.loading = true
         try {
