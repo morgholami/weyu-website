2be2563d9ee15d2ac66ed2b78b7bd2d0ed768ef1
Weyu Team <team@weyu.io>
2021-05-31 12:38:04 +0200

    fix telegram link

--- components/Footer.vue
+++ components/Footer.vue
@@ -5,7 +5,7 @@
         <div class="px-2">
           <a href="https://twitter.com/weyuofficial" target="_blank" class="subtitle is-5 mx-2"><i class="fab fa-twitter"></i></a>
           <a href="https://instagram.com/weyuofficial" target="_blank" class="subtitle is-5 mx-2"><i class="fab fa-instagram"></i></a>
-          <a href="https://t.me/weyuofficial" target="_blank" class="subtitle is-5 mx-2"><i class="fab fa-telegram"></i></a>
+          <a href="https://t.me/WEYUchat" target="_blank" class="subtitle is-5 mx-2"><i class="fab fa-telegram"></i></a>
           <a href="https://github.com/weyuio" target="_blank" class="subtitle is-5 mx-2"><i class="fab fa-github"></i></a>
         </div>
         <p class="mt-5">Powered by:</p>
